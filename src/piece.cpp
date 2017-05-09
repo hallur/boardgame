59dbdf3537fb01bbf7397770dbbbf4b8c5676415
Hallur A. Atlason <halluratlason@gmail.com>
2017-05-09 14:51:42 +0000

    Initialized moves_ and killMoves_ vectors with default behaviour

--- src/piece.cpp
+++ src/piece.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 
 Piece::Piece(const char& marker, std::string* player) : marker_(marker), player_(player) {
-    //moves_.push_back(std::make_pair(0, -1));
+    moves_ = {
+        {0, -1}, // up
+        {1, 0},  // right
+        {0, 1},  // down
+        {-1, 0}  // left
+    };
+
+    killMoves_ = {
+        {1, -1}, // up-right
+        {1, 1},  // down-right
+        {-1, 1}, // down-left
+        {-1, -1} // up-left
+    };
 }
 
 Piece::~Piece() {}
