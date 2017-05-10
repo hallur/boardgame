9495c7558bdbe935bfc5563c835c6063af11478a
dadisigursveinn <dadi.sigursveinn@gmail.com>
2017-05-10 15:19:51 +0000

    Added definitions for piece functions

--- src/piece.cpp
+++ src/piece.cpp
@@ -5,6 +5,18 @@ boardgame::Piece::Piece(const char& marker, boardgame::Player* player) : marker_
 
 boardgame::Piece::~Piece() {}
 
+boardgame::Player* boardgame::Piece::getPlayer() const {
+    return player_;
+}
+
+void boardgame::Piece::setPlayer(boardgame::Player* player) {
+    player_ = player; 
+}
+
+std::vector<boardgame::MoveRule> boardgame::Piece::getMoveRules() const {
+    return moveRules_;
+}
+
 std::ostream& boardgame::operator<<(std::ostream& os, const boardgame::Piece& rhs) {
     os << rhs.marker_;
     return os;
