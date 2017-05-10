ceb84a4188a80b0fe82c92e2cee92e95ee19a4dd
dadisigursveinn <dadi.sigursveinn@gmail.com>
2017-05-10 15:05:13 +0000

    Uodated piece.cpp, removed error from old constructor

--- src/piece.cpp
+++ src/piece.cpp
@@ -1,21 +1,7 @@
 #include "piece.h"
 #include <iostream>
 
-boardgame::Piece::Piece(const char& marker, boardgame::Player* player) : marker_(marker), player_(player) {
-    moves_ = {
-        {0, -1}, // up
-        {1, 0},  // right
-        {0, 1},  // down
-        {-1, 0}  // left
-    };
-
-    killMoves_ = {
-        {1, -1}, // up-right
-        {1, 1},  // down-right
-        {-1, 1}, // down-left
-        {-1, -1} // up-left
-    };
-}
+boardgame::Piece::Piece(const char& marker, boardgame::Player* player) : marker_(marker), player_(player) {}
 
 boardgame::Piece::~Piece() {}
 
