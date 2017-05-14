391cf1f07e85e9f382a654870f0feaea9d862cae
dadisigursveinn <dadi.sigursveinn@gmail.com>
2017-05-14 17:50:59 +0000

    Making megabreakthrough inherit breakthrough

--- include/megabreakthrough/board.h
+++ include/megabreakthrough/board.h
@@ -1,20 +1,17 @@
 #ifndef MEGABREAKTHROUGH_BOARD_H
 #define MEGABREAKTHROUGH_BOARD_H
 
-#include "../board.h"
+#include "../breakthrough/board.h"
 
 namespace boardgame {
 namespace megabreakthrough {
 
-class Board : public boardgame::Board {
+class Board : public boardgame::breakthrough::Board {
 public:
     explicit Board(Player* player1, Player* player2, int width, int height);
-    explicit Board(const megabreakthrough::Board&);
     virtual ~Board();
 
     virtual void initialize(Player* player1, Player* player2);
-
-    Player* playerOnWinningTile(Player* player1, Player* player2);
 };
 
 }}
--- src/megabreakthrough/board.cpp
+++ src/megabreakthrough/board.cpp
@@ -1,16 +1,14 @@
 #include "megabreakthrough/board.h"
-#include "megabreakthrough/pawn.h"
+#include "breakthrough/pawn.h"
 #include "megabreakthrough/megapawn.h"
 #include "exceptions.h"
 
-boardgame::megabreakthrough::Board::Board(boardgame::Player* player1, boardgame::Player* player2, int width, int height) : boardgame::Board(width, height) {
-    if (height < 4 || width < 1) {
+boardgame::megabreakthrough::Board::Board(boardgame::Player* player1, boardgame::Player* player2, int width, int height) : boardgame::breakthrough::Board(player1, player2, width, height) {
+    /*if (height < 4 || width < 1) {
         throw illegal_board_dimentions_exception();
-    }
+    }*/
     initialize(player1, player2);
 }
-
-boardgame::megabreakthrough::Board::Board(const boardgame::megabreakthrough::Board& rhs) : boardgame::Board::Board(rhs) {}
 
 boardgame::megabreakthrough::Board::~Board() {}
 
@@ -26,7 +24,7 @@ void boardgame::megabreakthrough::Board::initialize(boardgame::Player* player1, 
     // place new pieces in initial positions
     for (int x = 0; x < width_; x++) { // row 1 and 2
         pieces_[0][x] = new boardgame::megabreakthrough::MegaPawn(player2, true);
-        pieces_[1][x] = new boardgame::megabreakthrough::Pawn(player2, true);
+        pieces_[1][x] = new boardgame::breakthrough::Pawn(player2, true);
     }
     for (int y = 2; y < height_ - 2; y++) { // row 3 to (bottom - 2)
         for (int x = 0; x < width_; x++) {
@@ -34,23 +32,7 @@ void boardgame::megabreakthrough::Board::initialize(boardgame::Player* player1, 
         }
     }
     for (int x = 0; x < width_; x++) { // row bottom and bottom - 1
-        pieces_[height_-2][x] = new boardgame::megabreakthrough::Pawn(player1, false);
+        pieces_[height_-2][x] = new boardgame::breakthrough::Pawn(player1, false);
         pieces_[height_-1][x] = new boardgame::megabreakthrough::MegaPawn(player1, false);
     }
-}
-
-boardgame::Player* boardgame::megabreakthrough::Board::playerOnWinningTile(boardgame::Player* player1, boardgame::Player* player2) {
-    for (int x = 0; x < width_; x++) { // row 1 and 2
-        if ( pieces_[0][x] ) {
-            if ( pieces_[0][x]->getPlayer() == player1) {
-                return player1;
-            }
-        }
-        if ( pieces_[height_-1][x] ) {
-            if (pieces_[height_-1][x]->getPlayer() == player2) {
-                return player2;
-            }
-        }
-    }
-    return nullptr;
 }
